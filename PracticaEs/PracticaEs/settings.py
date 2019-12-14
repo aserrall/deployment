625ee127713ddcb7ca7dc22b45beb7c5337bd959
aserrall <andreu_s_ll@hotmail.com>
2019-12-14 19:24:16 +0100

    .

--- PracticaEs/PracticaEs/settings.py
+++ PracticaEs/PracticaEs/settings.py
@@ -75,6 +75,8 @@ WSGI_APPLICATION = 'PracticaEs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
